77b7aae80d28f3e8f55ace24e0bc95b5d3f2d561
IremTektas <tektasirem13@gmail.com>
2023-03-08 21:39:11 +0300

    Initial Commit

---
+++ SimpleFoursquareClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleFoursquareClone
+//
+//  Created by iremt on 8.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

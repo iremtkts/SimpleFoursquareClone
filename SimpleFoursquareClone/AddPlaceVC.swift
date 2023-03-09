702dcba2f37d9f8b4e02410672614bdcc6dcbbbb
IremTektas <tektasirem13@gmail.com>
2023-03-09 21:18:06 +0300

    add place view controller added

---
+++ SimpleFoursquareClone/AddPlaceVC.swift
@@ -0,0 +1,30 @@
+//
+//  AddPlaceVC.swift
+//  SimpleFoursquareClone
+//
+//  Created by iremt on 9.03.2023.
+//
+
+import UIKit
+
+class AddPlaceVC: UIViewController {
+    @IBOutlet weak var placeNameText: UITextField!
+    
+    @IBOutlet weak var placeTypeText: UITextField!
+    
+    @IBOutlet weak var placeAtmosphereText: UITextField!
+    
+    @IBOutlet weak var placeImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+    @IBAction func nextButtonClicked(_ sender: Any) {
+    }
+    
+}

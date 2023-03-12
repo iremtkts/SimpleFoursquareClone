a44a81056880e94d78945d8315ce471c7f0852d5
IremTektas <tektasirem13@gmail.com>
2023-03-12 20:25:32 +0300

    fixed

--- SimpleFoursquareClone/DetailVC.swift
+++ SimpleFoursquareClone/DetailVC.swift
@@ -9,7 +9,7 @@ import UIKit
 import MapKit
 import Parse
 
-class DetailVC: UIViewController {
+class DetailVC: UIViewController , MKMapViewDelegate {
 
     @IBOutlet weak var detailImageView: UIImageView!
     
@@ -29,7 +29,7 @@ class DetailVC: UIViewController {
         super.viewDidLoad()
         
         getDataFromParse()
-        
+        detailMapView.delegate = self
     }
     
     func getDataFromParse () {
@@ -43,6 +43,8 @@ class DetailVC: UIViewController {
                 if objects != nil {
                     if objects!.count > 0 {
                         let chosenPlaceObject = objects![0]
+                        
+                        //objects
                         
                         if let placeName = chosenPlaceObject.object(forKey: "name") as? String {
                             self.detailPlaceNameLabel.text = placeName
@@ -78,10 +80,72 @@ class DetailVC: UIViewController {
                                 
                             }
                         }
+                        
+                        
+                        // maps
+                        
+                        let location = CLLocationCoordinate2D(latitude: self.chosenLatitude, longitude: self.chosenLongitude)
+                        
+                        let span = MKCoordinateSpan(latitudeDelta: 0.035, longitudeDelta: 0.035)
+                        
+                        let region = MKCoordinateRegion(center: location, span: span)
+                        
+                        self.detailMapView.setRegion(region, animated: true)
+                        
+                        let annotation = MKPointAnnotation()
+                        annotation.coordinate = location
+                        annotation.title = self.detailPlaceNameLabel.text!
+                        annotation.subtitle = self.detailPlaceTypeLabel.text!
+                        self.detailMapView.addAnnotation(annotation)
                     }
                 }
             }
         }
+           
+        func mapView(_ mapView: MKMapView, viewForAnnotation annotation : MKAnnotation) -> MKAnnotationView? {
+            if annotation is MKUserLocation {
+                return nil
+            }
+            
+            let reuseId = "pin"
+            
+            var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseId)
+            
+            if pinView == nil {
+                pinView = MKAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+                pinView?.canShowCallout = true
+                pinView?.tintColor = .systemPink
+                let button = UIButton(type: .detailDisclosure)
+                pinView?.rightCalloutAccessoryView = button
+            } else {
+                pinView?.annotation = annotation
+            }
+            return pinView
+        }
+
+        
+        func mapView(_ mapView:MKMapView,annotationView view:MKAnnotationView,calloutAccessoryControlTapped control:UIControl) {
+            if self.chosenLongitude != 0.0 && self.chosenLatitude != 0.0 {
+                let requestLocation = CLLocation(latitude: self.chosenLatitude, longitude: self.chosenLongitude)
+                
+                CLGeocoder().reverseGeocodeLocation(requestLocation) { placemarks , error in
+                    if let placemark = placemarks {
+                        if placemark.count > 0 {
+                            let mkPlaceMark = MKPlacemark(placemark: placemark[0])
+                            let mapItem = MKMapItem(placemark: mkPlaceMark)
+                            mapItem.name = self.detailPlaceNameLabel.text
+                            
+                            let launchOptions = [MKLaunchOptionsDirectionsModeKey : MKLaunchOptionsDirectionsModeDriving]
+                            
+                            mapItem.openInMaps(launchOptions: launchOptions)
+                        }
+                    }
+                    
+                }
+            }
+        }
+        
+        
     }
     
 
